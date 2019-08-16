a5ca82a1e3febb3cff762c7c718ced6cc0b181b9
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-08-16 18:58:05 -0400

    Pretty cool spiralish symbol.

--- src/app_sketch/core.clj
+++ src/app_sketch/core.clj
@@ -5,17 +5,22 @@
 
 (defn draw []
   ; make background white
-  (q/background 255)
+  (q/background 235)
+  (q/stroke-weight 3)
+  (q/stroke 255 0 0 120)
+  (q/shininess 2)
+  (q/emissive 0 0 255)
+  (q/fill 130 89 209
 
   ; move origin point to centre of the sketch
   ; by default origin is in the left top corner
-  (q/with-translation [(/ (q/width) 2) (/ (q/height) 2)]
-   ; parameter t goes 0, 0.01, 0.02, ..., 99.99, 100
-   (doseq [t (range 2 130 0.01)]
-     ; draw a point with x = t * sin(t) and y = t * cos(t)
-     (q/point (* t (q/sin t))
-              (* t (q/cos t))))))
-; run sketch
+    (q/with-translation [(/ (q/width) 2) (/ (q/height) 2)]
+      ; parameter t goes 0, 0.01, 0.02, ..., 99.99, 100
+      (doseq [t (range 4 190 0.71)]
+        ; draw a point with x = t * sin(t) and y = t * cos(t)
+        (q/point (* t (q/sin t))
+                 (* t (q/cos t)))))))
+  ; run sketch
 (q/defsketch trigonometry
-  :size [900 300]
+  :size [900 900]
   :draw draw)
