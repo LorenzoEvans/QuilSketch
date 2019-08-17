01a247063803e8623fb8718d99684863a79f24f8
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-08-16 23:16:33 -0400

    Infinity flower thing.

--- src/app_sketch/animations.clj
+++ src/app_sketch/animations.clj
@@ -20,14 +20,14 @@
       (q/line (f t)
               (f (+ t 0.1)))
       (q/line (f t)
-              (f (+ t 0.1))))))
+              (f (+ t (* 2 -0.5)))))))
 
 ; 'setup' is a cousin of 'draw' function
 ; setup initialises sketch and it is called only once
 ; before draw called for the first time
 (defn setup []
   ; draw will be called 60 times per second
-  (q/frame-rate 190)
+  (q/frame-rate 290)
   (q/height 190)
   (q/width)
   (q/stroke 89 56 33)
@@ -35,7 +35,25 @@
   ; otherwise each invocation of 'draw' would clear sketch completely
   (q/background 255))
 
+
+
+(defn flower [t]
+  (let [r (* 500 (q/sin t) (q/cos t))]
+    [(* r (q/sin (* t 0.2)))
+     (* r (q/cos (* t 2)))]))
+
+(defn flower-line [t]
+  (let [r (* 200 (q/sin t) (q/cos t))]))
+
+
+(defn draw-animation []
+  (q/with-translation [(/ (q/width) 2) (/ (q/height) 2)]
+    (let [t (/ (q/frame-count) 10)]
+      ; set color for line
+      (q/stroke (mod t 10) 30 1)
+      (q/line (flower t) (flower (+ t 0.5))))))
+
 (q/defsketch trigonometry
   :size [900 900]
   :setup setup
-  :draw drawer)
+  :draw draw-animation)
